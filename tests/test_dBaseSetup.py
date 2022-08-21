2976a20ebb644a871b03e8db9ea2a8c929694649
only1abu <berhen18@students.ecu.edu>
2022-08-21 17:57:20 -0400

    Source code put inside /src directory along with unit tests in /tests directory

---
+++ tests/test_dBaseSetup.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class test_dBaseSetup(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_etfToSchema(self):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_inflationProjectMain.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class test_inflationProjectMain(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_getTables(self):
+
+    def test_getPrices(self):
+
+    def test_MACDindicator(self):
+
+    def test_applyMACD
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
